e73b5df95111953e76dda15cfe83504c79915839
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-15 15:18:03 -0400

    Types of strings

---
+++ src/strings.ts
@@ -0,0 +1,11 @@
+// Exemplo 01 - Aspas simples
+let nomeSobrenome: string = 'Thiago Hens Suchi';
+console.log(nomeSobrenome);
+
+// Exemplo 02 - Aspas Duplas
+let frutaVermelha: string = "Morango, Maça, Jambo";
+console.log(frutaVermelha);
+
+// Exemplo 03 - Crases
+let interpolacao: string = `Me chamo ${nomeSobrenome}, as frutas que mais gosto da cor vermelha são: ${frutaVermelha}`
+console.log(interpolacao);
