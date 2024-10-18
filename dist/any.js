e4d27ed7869b422588550885aff045321f221051
THIAGO <thiagohenssuchi@gmail.com>
2024-10-18 18:51:21 -0400

    understanding the any

---
+++ dist/any.js
@@ -0,0 +1,6 @@
+"use strict";
+//------------------- Any ----------------------
+let nomeValor = 'Rebeca';
+let idadeValor = 21;
+const juncao = nomeValor + idadeValor;
+console.log(juncao);
---
+++ src/any.ts
@@ -0,0 +1,7 @@
+//------------------- Any ----------------------
+let nomeValor: any = 'Rebeca';
+let idadeValor: any = 21;
+
+const juncao = nomeValor + idadeValor;
+
+console.log(juncao);
