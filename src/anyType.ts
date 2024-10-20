8f9506e13905e996114a56a01533e3f5880ccfe3
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-20 16:24:49 -0400

    type any and when to use

---
+++ src/anyType.ts
@@ -0,0 +1,17 @@
+//--------------- ANY -----------------
+
+// Exemplo 01 => Type Any
+const number: any = 30;
+const nombre: any = ['MrBreaks'];
+
+const result = number + nombre
+console.log(result);
+
+// Exemplo 02 => Quando devemos usar o tipo any?
+const formulario: {[campoFormulario: string]: any} = {
+    nome: 'Thiago',
+    sobrenome: 'Hens',
+    idade: 21
+};
+
+console.log(formulario);
