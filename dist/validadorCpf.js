269bb4e80b57bfdcb07475a2d2e941e5608a7af7
THIAGO <thiagohenssuchi@gmail.com>
2024-10-24 17:34:51 -0400

    criando a variavel para transformar o cpf em number

--- dist/validadorCpf.js
+++ dist/validadorCpf.js
@@ -1,5 +1,12 @@
 "use strict";
 function validarCpf(cpf) {
-    console.log(parseInt(cpf[0]) * 10);
+    let number = parseInt(cpf[0]);
+    console.log(number);
+    // let decrescente: Array<number> = [10,9,8,7,6,5,4,3,2];
+    // let soma:number = 0;
+    // for(let ii = 0; ii < converesor.length)
+    // for(let i = 0; i < decrescente.length; i++){
+    //     console.log(decrescente[i]);
+    // }
 }
-validarCpf("051.902.292-03");
+validarCpf("05190229203");
--- src/validadorCpf.ts
+++ src/validadorCpf.ts
@@ -1,13 +1,21 @@
 function validarCpf(cpf: string) {
+    let number:number = parseInt(cpf[0])
+   
+    let decrescente: Array<number> = [10,9,8,7,6,5,4,3,2];
+    let soma:number = 0;
 
-    let soma: number = 0
-    for (let i = 0; i < 9; i++) {
+    for(let ii = 0; ii < converesor.length) {
         
     }
 
-    console.log(parseInt(cpf[0]) * 10);
+    for(let i = 0; i < decrescente.length; i++){
+        console.log(decrescente[i]);
+           
+    }
+    
+    
 }
 
 
-validarCpf("051.902.292-03")
+validarCpf("05190229203")
 
