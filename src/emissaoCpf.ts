06d978884e04dce05648d51f880d7d0ca3838715
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-26 17:50:58 -0400

    algumas alterações a respeito do if/ foi adcionado alguns comentarios

--- src/emissaoCpf.ts
+++ src/emissaoCpf.ts
@@ -1,9 +1,10 @@
-let cpf = "05190229203"
+let cpf = "76731503606"
 
 function estadoEmissor(Cpf:string) {
     
-    let converter = Cpf.split("").map(Number)
+    let convertido = Cpf.split("").map(Number)
 
+    // Criei um objeto para listar os Estados em seus respectivos números
     let numEstado: { num0: string; num1: string; num2: string; num3: string; num4: 
     string; num5: string; num6: string; num7: string; num8: string; num9: string} = {
         num0: "RS",
@@ -18,12 +19,29 @@ function estadoEmissor(Cpf:string) {
         num9: "PR e SC"    
     }
 
-    for(let i = 0; i < converter.length; i++){
-        switch
-    }
-
-   
-    
+    // Condição para filtrar qual Estado emitiu o cpf, de acordo com o número
+    if(convertido[8] === 0) {
+        return `Seu Estado de emissão é ${numEstado.num0}`;
+    } else if(convertido[8] === 1) {
+        return `Seu Estado de emissão é ${numEstado.num1}`;
+    } else if(convertido[8] === 2) {
+        return `Seu Estado de emissão é ${numEstado.num2}`;
+    } else if(convertido[8] === 3) {
+        return `Seu Estado de emissão é ${numEstado.num3}`;
+    } else if(convertido[8] === 4) {
+        return `Seu Estado de emissão é ${numEstado.num4}`;
+    } else if(convertido[8] === 5) {
+        return `Seu Estado de emissão é ${numEstado.num5}`;
+    } else if(convertido[8] === 6) {
+        return `Seu Estado de emissão é ${numEstado.num6}`;
+    } else if(convertido[8] === 7) {
+        return `Seu Estado de emissão é ${numEstado.num7}`;
+    } else if(convertido[8] === 8) {
+        return `Seu Estado de emissão é ${numEstado.num8}`;
+    } else if(convertido[8] === 9) {
+        return `Seu Estado de emissão é ${numEstado.num9}`;
+    } 
 } 
 
 console.log(estadoEmissor(cpf));
+ 
