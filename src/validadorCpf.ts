eb73688491f9414ab7b2d1718519f766563654c3
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-26 16:34:36 -0400

    Função validadora de cpf - retornando true se o cpf for valido e false se não

--- src/validadorCpf.ts
+++ src/validadorCpf.ts
@@ -1,50 +1,65 @@
-function validarCpf(cpf: string) {
-    // Primeiro dígito verificador
-    let arrayMultiplicado: Array<number> = []
-    let multiplicando: number = 10
-    let soma: number = 0
-   
+function validarCpf(cpf: string):boolean {
+    let conversor = cpf.split("").map(Number)
 
-    // Aqui eu iterei os 9 primeiros numeros e multipliquei por 10 - 2
-    for (let i = 0; i < 9; i++) {
-        arrayMultiplicado.push(parseInt(cpf[i]) * multiplicando)
-        multiplicando--
+    let digitoVerificador: number = 0
+    let digitoVerificador2: number = 0
+
+    if (conversor.length == 11) {
+        // Primeiro dígito verificador
+
+        let arrayMultiplicado: Array<number> = []
+        let multiplicando: number = 10
+        let soma: number = 0
+
+        // Aqui eu iterei os 9 primeiros numeros e multipliquei por 10 - 2
+        for (let i = 0; i < 9; i++) {
+            arrayMultiplicado.push(conversor[i] * multiplicando)
+            multiplicando--
+        }
+
+        // Somei todos os numeros do arrayMultiplicado
+        for (let j = 0; j < arrayMultiplicado.length; j++) {
+            soma += arrayMultiplicado[j]
+        }
+
+        // Verificando o primero DV
+        digitoVerificador = soma % 11
+
+
+        //-------------------------------------------------------------------------    
+
+        // Segundo dígito verificador
+
+        let arrayMultiplicado2: Array<number> = []
+        let multiplicando2: number = 11
+        let soma2: number = 0
+
+        for (let i = 0; i < 10; i++) {
+            arrayMultiplicado2.push(parseInt(cpf[i]) * multiplicando2)
+            multiplicando2--
+        }
+
+        for (let j = 0; j < arrayMultiplicado2.length; j++) {
+            soma2 += arrayMultiplicado2[j]
+        }
+
+        // Aqui como o segundo DV não foi 0 nem 1, logo se subtraí por 11
+        let resto2: number = soma2 % 11
+        digitoVerificador2 = 11 - resto2
+
+        if (digitoVerificador2 == conversor[10] && digitoVerificador == conversor[9]) {
+            return true
+        } else {
+            return false
+        }
+
+    } else {
+        return false
     }
 
-    // Somei todos os numeros do arrayMultiplicado
-    for (let j = 0; j < arrayMultiplicado.length; j++) {
-        soma += arrayMultiplicado[j]
-    }
-
-    // Verificando o primero DV
-    let resto = soma % 11
-    console.log(`O primeiro dígito verificador é -> ${resto}`);
-
-//-------------------------------------------------------------------------    
-
-    // Segundo dígito verificador
-    let arrayMultiplicado2: Array<number> = []
-    let multiplicando2: number = 11
-    let soma2:number = 0
-    
-    
-    
-    for (let i = 0; i < 10; i++) {
-        arrayMultiplicado2.push(parseInt(cpf[i]) * multiplicando2)
-        multiplicando2--
-    }
-
-    for (let j = 0; j < arrayMultiplicado2.length; j++) {
-        soma2 += arrayMultiplicado2[j]
-    }
-
-    // Aqui como o segundo DV não foi 0 nem 1, logo se subtraí por 11
-    let resto2:number = soma2 % 11
-    let subtracao:number = 11 - resto2
-    console.log(`O segundo dígito verificador é -> ${subtracao}`);
-    
 }
 
 
-validarCpf("05190229203")
+console.log(validarCpf("76731503606"));
+ 
 
